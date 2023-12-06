dc3ac84b9232981768bab6b12300e9198f26a197
Your Name <you@example.com>
2023-12-06 18:00:11 +0600

    fixed web3 init problem

--- src/components/App.js
+++ src/components/App.js
@@ -1,72 +1,84 @@
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { loadProvider, loadAccount, loadNetwork, loadTokens, loadExchange, loadAllOrders, subscribeToEvents } from '../store/interactions'
-import config from '../config.json'
-import Navbar from './Navbar'
-import Markets from './Markets'
-import Balance from './Balance'
-import Order from './Order'
-import PriceChart from './PriceChart'
-import OrderBook from './OrderBook'
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import {
+  loadProvider,
+  loadAccount,
+  loadNetwork,
+  loadTokens,
+  loadExchange,
+  loadAllOrders,
+  subscribeToEvents,
+} from '../store/interactions';
+import config from '../config.json';
+import Navbar from './Navbar';
+import Markets from './Markets';
+import Balance from './Balance';
+import Order from './Order';
+import PriceChart from './PriceChart';
+import OrderBook from './OrderBook';
 
 function App() {
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
 
-  const dispatch = useDispatch()
+  const loadBlockchainData = async () => {
+    try {
+      const provider = loadProvider(dispatch);
+      const chainId = await loadNetwork(provider, dispatch);
 
-  const loadBlockchainData = async() => {
+      window.ethereum.on('chainChanged', () => {
+        window.location.reload();
+      });
+      window.ethereum.on('accountsChanged', async () => {
+        await loadAccount(provider, dispatch);
+      });
 
-    const provider = loadProvider(dispatch)
-    const chainId = await loadNetwork(provider, dispatch)
-    
-    window.ethereum.on('chainChanged', () => {window.location.reload()})
-    window.ethereum.on('accountsChanged', async() => {await loadAccount(provider, dispatch)})
-    
-    await loadTokens(provider, [config[chainId].RH.address, config[chainId].mETH.address], dispatch)
-    
-    const exchange = loadExchange(provider, config[chainId].exchange.address, dispatch)
+      await loadTokens(provider, [config[chainId].RH.address, config[chainId].mETH.address], dispatch);
 
-    await loadAllOrders(provider, exchange, dispatch)
+      const exchange = loadExchange(provider, config[chainId].exchange.address, dispatch);
 
-    subscribeToEvents(exchange, dispatch)
-  }
+      await loadAllOrders(provider, exchange, dispatch);
+
+      subscribeToEvents(exchange, dispatch);
+
+      // Set loading to false once all data is loaded
+      setLoading(false);
+    } catch (error) {
+      console.error('Error loading blockchain data:', error);
+      // Handle error loading blockchain data
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    loadBlockchainData()
-  })
+    loadBlockchainData();
+  }, [loadBlockchainData]); // Include loadBlockchainData in the dependency array
 
   return (
     <div>
-
       <Navbar />
 
-      <main className='exchange grid'>
+      {loading ? (
+        // Display a loading animation while the data is being loaded
+        <div>Loading...</div>
+      ) : (
+        <main className='exchange grid'>
+          <section className='exchange__section--left grid'>
+            <Markets />
+            <Balance />
+            <Order />
+          </section>
 
-        <section className='exchange__section--left grid'>
-
-          <Markets />
-
-          <Balance />
-
-          <Order />
-
-        </section>
-
-        <section className='exchange__section--right grid'>
-
-          <PriceChart />
-
-          {/* Transactions */}
-
-          {/* Trades */}
-
-          <OrderBook />
-
-        </section>
-        
-      </main>
+          <section className='exchange__section--right grid'>
+            <PriceChart />
+            {/* Transactions */}
+            {/* Trades */}
+            <OrderBook />
+          </section>
+        </main>
+      )}
 
       {/* Alert */}
-
     </div>
   );
 }
--- src/components/Markets.js
+++ src/components/Markets.js
@@ -1,40 +1,69 @@
-import { useSelector, useDispatch } from 'react-redux'
-import config from '../config.json'
-import { loadTokens } from '../store/interactions'
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import config from '../config.json';
+import { loadTokens } from '../store/interactions';
 
 const Markets = () => {
-    
-    const chainId = useSelector(state => state.provider.chainId)
-    const provider = useSelector(state => state.provider.connection)
-    const dispatch = useDispatch()
+  const [loading, setLoading] = useState(false);
+  const chainId = useSelector((state) => state.provider.chainId);
+  const provider = useSelector((state) => state.provider.connection);
+  const dispatch = useDispatch();
 
-    const marketHandler = async(e) => {
-        
-        loadTokens(provider, e.target.value.split(','), dispatch)
-    }
+  const marketHandler = async (e) => {
+    setLoading(true);
 
-    return(
-    
+    // Simulate a delay (e.g., 1 second) to show the loading animation
+    setTimeout(async () => {
+      try {
+        await loadTokens(provider, e.target.value.split(','), dispatch);
+      } catch (error) {
+        console.error('Error loading tokens:', error);
+      } finally {
+        setLoading(false);
+      }
+    }, 1000);
+  };
+
+  useEffect(() => {
+    // Check Web3 provider connectivity
+    const checkProviderConnection = async () => {
+      try {
+        // Perform a simple Web3 provider connectivity check
+        const accounts = await provider.listAccounts();
+
+        // If accounts are retrieved successfully, the provider is connected
+        console.log('Web3 provider is connected:', accounts);
+      } catch (error) {
+        // Handle the error if the provider is not connected
+        console.error('Web3 provider is not connected:', error);
+      }
+    };
+
+    // Call the function to check Web3 provider connectivity
+    checkProviderConnection();
+
+    // Additional setup or side effects can be added here
+    // This empty dependency array means this effect runs once on mount
+  }, [provider]); // Include provider in the dependency array
+
+  return (
     <div className='component exchange__markets'>
-        
-        <div className='component__header'>
-            <h2>Select Market</h2>
-        </div>
+      <div className='component__header'>
+        <h2>Select Market</h2>
+      </div>
 
-        {
-            config[chainId] &&
-            (
-                <select name='markets' id='markets' onChange={(e) => marketHandler(e)}>
-                    <option value={`${config[chainId].RH.address},${config[chainId].mETH.address}`}>RH / mETH</option>
-                    <option value={`${config[chainId].RH.address},${config[chainId].mDAI.address}`}>RH / mDAI</option>
-                </select>
-            )
-        }
+      {loading && <div>Loading...</div>}
 
-        <hr />
+      {config[chainId] && (
+        <select name='markets' id='markets' onChange={(e) => marketHandler(e)}>
+          <option value={`${config[chainId].RH.address},${config[chainId].mETH.address}`}>RH / mETH</option>
+          <option value={`${config[chainId].RH.address},${config[chainId].mDAI.address}`}>RH / mDAI</option>
+        </select>
+      )}
 
+      <hr />
     </div>
-    )
-}
+  );
+};
 
-export default Markets
+export default Markets;
