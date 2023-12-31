8c2724e6d8e08b2fe6dca51eab3fcf9c09f5581f
16rakib16 <16rakib16@gmail.com>
2023-12-31 07:26:37 +0600

    39. Deploying to Ethereum Sepolia & Polygon Mumbai Test nets

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,23 @@
-require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  
   solidity: "0.8.19",
+  
   networks: {
-    localhost: {}
+    
+    localhost: {},
+    
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: process.env.SEPOLIA_PRIVATE_KEYS.split(',')
+    },
+    
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: process.env.MUMBAI_PRIVATE_KEYS.split(',')
+    }
   },
 };
