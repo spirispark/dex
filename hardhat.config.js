507d6dda3770188feaa369b1ad47e66e81afc058
16rakib16 <16rakib16@gmail.com>
2023-12-31 23:48:18 +0600

    40. Fixing Test account & Contract addresses for Ethereum Sepolia & Polygon Mumbai Test Nets

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,12 +12,12 @@ module.exports = {
     
     sepolia: {
       url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: process.env.SEPOLIA_PRIVATE_KEYS.split(',')
+      accounts: process.env.PRIVATE_KEYS.split(',')
     },
     
     mumbai: {
       url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: process.env.MUMBAI_PRIVATE_KEYS.split(',')
+      accounts: process.env.PRIVATE_KEYS.split(',')
     }
-  },
-};
+  }
+}
